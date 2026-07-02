b1ab40793f10797392b7fec427e90faa0af2703c
Dima <dima.email@example.com>
2026-07-02 15:43:40 +0300

    exam to-do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,82 @@
-import { useState, useEffect } from 'react'
-import { v4 as uuidv4 } from 'uuid';
-import { taskList } from './data/taskList'
-import TaskRender from './components/TaskRender'
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { taskList } from './data/taskList';
+import TaskRender from './components/TaskRender';
 import './App.css'
 
+
 function App() {
-    const [tasks, setTask] = useState(taskList);
-    const [inputValue, setInputValue] = useState('');
+    const [task, setTask] = useState(taskList);
+
+    const [inputHeader, setInputHeader] = useState('');
+    const [inputText, setInputText] = useState('');
 
     function addTask(e) {
         e.preventDefault();
 
-        setTask(n => [...tasks,{
-            id: uuidv4(), 
-            text: inputValue,
-            completed: false
-        }
-        ])
+        setTask([...task,{
+            id: uuidv4(),
+            header: inputHeader,
+            text: inputText,
+            important: false
+        }])
 
-        setInputValue('');
+        setInputHeader('');
+        setInputText('');
+    }
+    
+    function toggleImportant(id) {
+        setTask(n => n.map(task =>
+            task.id === id ?
+            {...task, important: !task.important } : task
+        ))
     }
 
-    function toggleCompleted(id) {
-        setTask(n => n.map((task) =>
-                task.id === id ?
-                { ...task, completed: !task.completed } : task
-            )      
-        )
+    function deleteTask(id) {
+        setTask(n => n.filter(task => 
+            task.id !== id 
+        ))
+    }
+
+    function filteredTask() {
+        setTask(n => n.filter(task =>
+            task.important
+        ))
     }
 
     return (
         <>
             <div>
-                <h1>Список задач</h1>
+                <h1>
+                    Notes Manager
+                </h1>
 
-                {tasks.map(n => (
-                    <TaskRender
-                    key={n.id} 
-                    id={n.id} 
-                    text={n.text} 
-                    completed={n.completed} 
-                    onToggle={toggleCompleted}/>
+                {task.map(n => (
+                    <TaskRender 
+                        key={n.id}
+                        id={n.id}
+                        header={n.header}
+                        text={n.text}
+                        important={n.important}
+                        onToggle={toggleImportant}
+                        onDelete={deleteTask}
+                    />
                 ))}
 
                 <form onSubmit={addTask}>
-                    <input type='text' value={inputValue} onChange={e => setInputValue(e.target.value)} />
+                    <div>
+                        <div>Заголовок</div>
+                        <input type='text' value={inputHeader} onChange={e => setInputHeader(e.target.value)}/>
+
+                        <div>Текст</div>
+                        <input type='text' value={inputText} onChange={e => setInputText(e.target.value)}/>
+                    </div>
+                    
+
                     <button type='submit'>Добавить задачу</button>
                 </form>
+
+                <button onClick={() => filteredTask()}>Фильтровать</button>
             </div>
         </>
     )
--- src/components/TaskRender.jsx
+++ src/components/TaskRender.jsx
@@ -1,13 +1,21 @@
-function TaskRender({id, text, completed, onToggle}) {
+import classNames from 'classnames'
+import '../App.css'
+
+function TaskRender({ id, header, text, important, onToggle, onDelete }) {
+    const colorImportant = classNames('card', {
+        'important': important ? true : false
+    })
 
     return (
-        <div>
-            <ul>
-                {text}
-                <input type='checkbox' onChange={() => onToggle(id)}/>{completed ? 'Выполнено' : 'Не выпонено'}
-            </ul>
-            
-        </div>
+        <>
+            <div className={colorImportant}>
+                <h2>{header}</h2>
+                - {text}
+                
+                <input type="checkbox" onChange={() => onToggle(id)}/>{important ? 'Важно' : 'Не важно'}
+                <button onClick={() => onDelete(id)}>Удалить</button>
+            </div>
+        </>
     )
 }
 
